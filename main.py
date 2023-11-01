f65777e3c61d911c10a15af6bec5758f629f0945
janeu19 <84382514+janeu19@users.noreply.github.com>
2023-11-01 20:32:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,94 +1,19 @@
-import asyncio
-import websockets
+from flask import Flask, render_template, request
 
-PORT = 8080
+app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
 
-connected_clients = {}
-banned_ips = {}
-unban_time = 10
-client_id = 1
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('main.html'), 200
 
+@app.route('/registrace', methods=['GET', 'POST'])
+def registrace():
+    if request.method == 'POST':
+        # Zde můžete zpracovat data z formuláře, pokud budete chtít.
 
-async def handle_client(websocket, path):
-    global client_id
+        # Přesměrování zpět na úvodní stránku po odeslání formuláře
+        return render_template('main.html'), 200
+    return render_template('registrace.html'), 200
 
-    client_ip = websocket.remote_address[0]
-    print(f"Incoming IP: {client_ip}, current ID: {client_id}")
-
-    # Zkontroluje, zda je klient blokován.
-    if client_ip in banned_ips:
-        if not check_if_unban(client_ip):
-            return
-
-        # Odešle klientovi zprávu, že byl odblokován.
-        await websocket.send("SERVER_MESSAGE: Unbanned.")
-
-    # Přidá klienta do seznamu připojených klientů.
-    connected_clients[client_id] = websocket
-    client_id += 1
-
-    try:
-        async for message in websocket:
-            print(f"Received message from client: {message}")
-
-            # Zkontroluje, zda klient odeslal zakázané slovo.
-            if message == "pica":
-                await ban(websocket)
-                return
-
-            # Zjistí ID aktuálního klienta.
-            current_id = 0
-            for id, client in connected_clients.items():
-                if client == websocket:
-                    current_id = id
-                    break
-
-            # Odešle zprávu všem připojeným klientům.
-            for id, client in connected_clients.items():
-                print(f"Sending to {id}")
-                await client.send(f"{current_id} said: {message}")
-    except websockets.exceptions.ConnectionClosed as e:
-        print("A client just disconnected")
-    finally:
-        await disconnect(websocket)
-
-
-async def disconnect(websocket):
-    print("Disconnected")
-
-    # Odstraní klienta ze seznamu připojených klientů.
-    for id, client in connected_clients.items():
-        if client == websocket:
-            del connected_clients[id]
-            break
-
-    # Zavře připojení klienta.
-    await websocket.close()
-
-
-async def ban(websocket):
-    print("Banned")
-
-    # Odešle klientovi zprávu, že byl blokován.
-    await websocket.send("SERVER_MESSAGE: Banned.")
-
-    # Přidá klienta do seznamu blokovaných ip adres.
-    banned_ips[websocket.remote_address[0]] = time.time()
-
-    # Odstraní klienta ze seznamu připojených klientů.
-    await disconnect(websocket)
-
-
-def check_if_unban(client_ip):
-    if time.time() - banned_ips[client_ip] > unban_time:
-        del banned_ips[client_ip]
-        print(f"Unbanned: {client_ip}")
-        return True
-
-    return False
-
-
-if __name__ == "__main__":
-    start_server = websockets.serve(handle_client, "0.0.0.0", PORT)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
