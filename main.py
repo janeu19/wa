880501762a7db30d664a8ce99a0cb80405afa044
janeu19 <84382514+janeu19@users.noreply.github.com>
2023-11-12 20:19:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Rozsazení v lodích na vodáckém kurzu</title>
-    <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='mujstyl.css') }}">
-</head>
-<body>
-    <h1>Rozsazení v lodích na vodáckém kurzu 2023 SPŠE Ječná</h1>
-    <a href="/registrace">Registrovat se</a>
-    <h2>Seznam registrovaných osob</h2>
-    {% for user in users %}
-        <h3>{{ user.nick }}</h3>
-        <p>Plavec: {{ 'Ano' if user.je_plavec == '1' else 'Ne' }}</p>
-        <p>Kanoe kamarád: {{ user.kanoe_kamarad }}</p>
-    {% endfor %}
-</body>
-</html>
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Předpokládaná data
+data = {
+    "users": []
+}
+
+@app.route('/')
+def home():
+    return render_template('prvni_stranka.html', users=data['users'])
+
+@app.route('/registrace', methods=['GET', 'POST'])
+def registrace():
+    if request.method == 'POST':
+        nick = request.form.get('nick')
+        je_plavec = request.form.get('je_plavec')
+        kanoe_kamarad = request.form.get('kanoe_kamarad', '')
+
+        # Kontroly dat formuláře (zadání 2 a 4)
+        if not (je_plavec == '1' and 2 <= len(nick) <= 20 and (not kanoe_kamarad or 2 <= len(kanoe_kamarad) <= 20)):
+            return jsonify({'error': 'Neplatné údaje'}), 400
+
+        # Kontrola duplicity nickname (zadání 5)
+        if any(user['nick'] == nick for user in data['users']):
+            return jsonify({'error': 'Nickname již existuje'}), 400
+
+        data['users'].append({'nick': nick, 'je_plavec': je_plavec, 'kanoe_kamarad': kanoe_kamarad})
+        return jsonify({'success': 'Uživatel úspěšně zaregistrován'}), 200
+
+    return render_template('druha_stranka.html')
+
+@app.route('/api/check-nickname', methods=['GET'])
+def check_nickname():
+    nickname = request.args.get('nick')
+    exists = any(user['nick'] == nickname for user in data['users'])
+    return jsonify(exists=exists)
+
+if __name__ == '__main__':
+    app.run(debug=True)
